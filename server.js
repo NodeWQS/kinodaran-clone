6bb4d743cefd54e2d9c76f6041c13b7ce052b0e4
NodeWQS <coffeebreak096@gmail.com>
2022-08-04 18:50:00 +0400

    main

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-app.use(fileUpload());
+app.use(fileUpload({ limits: { fileSize: 15728640 }, abortOnLimit: true }));
 app.use('/movie', require('./movie/movie.router'));
 app.use('/photo', require('./photo/photo.router'));
 app.use('/category', require('./category/category.router'));
